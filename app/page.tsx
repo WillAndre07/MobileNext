0854734d0a5ca013a17289205887a13496dc62d6
Rafael <rafael@dev4ever.dev>
2023-12-03 22:06:16 -0300

    Segundo Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 
 export default function Home() {
-  const handleSignup = (event: { preventDefault: () => void } ) => {
-    event.preventDefault()
+  //const handleSignup = (event: { preventDefault: () => void } ) => {
+  //  event.preventDefault()
     
-  }
+  //}
   return (
     <main className="flex min-h-screen max-w-screen items-center justify-between p-24 flex-col">
       <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex flex-col my-5">
@@ -37,7 +37,7 @@ export default function Home() {
           <input type="email" name="email" id="email" placeholder='james@james.com' required className='rounded h-8 text-zinc-950 text-lg'/>
           <label htmlFor="telefone">Telefone</label>
           <input type="tel" name="telefone" id="telefone" placeholder='47 9 9999-9999' required className='rounded h-8 text-zinc-950 text-lg'/>
-          <button type="submit" className='rounded border-2 border-sky-200 h-10' onSubmit={handleSignup}>Enviar</button>
+          <button type="submit" className='rounded border-2 border-sky-200 h-10' >Enviar</button>
         </form>
         <p className="fixed left-0 bottom-32 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 my-5">
           Emergência ligue 188.&nbsp;
---
+++ pages/login.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Here, you can perform validation and authentication logic.
+    // For this example, let's assume a basic check for email and password.
+    if (email === 'user@example.com' && password === 'password') {
+      // Redirect to a dashboard or home page after successful login
+      router.push('/dashboard');
+    } else {
+      // Display an error message or handle authentication failure
+      console.log('Invalid credentials');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
